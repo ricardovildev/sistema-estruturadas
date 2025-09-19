2e8e4cf70376286824436fa024b6c2bee289d6f6
Jose Ricardo <ricardoviljunior@gmail.com>
2025-09-19 13:18:06 -0300

    Removi os arquivos consolidacao.py e teste.py

--- backend/consolidacao.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from backend.conexao import conectar
-
-def consolidar_dados():
-    engine = conectar()
-    query = "SELECT * FROM sua_tabela"
-    df = pd.read_sql(query, engine)
-    return df
