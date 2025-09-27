d2811462c3cfa9840b05f8871fd0ff9f84e605dd
ricardovildev <ricardoviljunior@gmail.com>
2025-09-27 15:32:31 -0300

    Update Cadastro_de_Usuarios.py

--- frontend/paginas/Cadastro_de_Usuarios.py
+++ frontend/paginas/Cadastro_de_Usuarios.py
@@ -16,20 +16,20 @@ def render():
 
 
 
-# 🧾 Formulário de cadastro
-st.title("👥 Cadastro de Usuários")
-
-nome = st.text_input("Nome completo")
-username = st.text_input("Nome de usuário")
-email = st.text_input("Email")
-senha = st.text_input("Senha", type="password")
-perfil = st.selectbox("Perfil", ["usuario", "admin"])
-
-if st.button("Cadastrar"):
-    if not nome or not username or not email or not senha:
-        st.warning("⚠️ Preencha todos os campos.")
-    elif usuario_existe(username):
-        st.error(f"❌ O usuário '{username}' já existe.")
-    else:
-        cadastrar_usuario(nome, username, senha, email, perfil)
-        st.success(f"✅ Usuário '{username}' cadastrado com sucesso.")
+    # 🧾 Formulário de cadastro
+    st.title("👥 Cadastro de Usuários")
+    
+    nome = st.text_input("Nome completo")
+    username = st.text_input("Nome de usuário")
+    email = st.text_input("Email")
+    senha = st.text_input("Senha", type="password")
+    perfil = st.selectbox("Perfil", ["usuario", "admin"])
+    
+    if st.button("Cadastrar"):
+        if not nome or not username or not email or not senha:
+            st.warning("⚠️ Preencha todos os campos.")
+        elif usuario_existe(username):
+            st.error(f"❌ O usuário '{username}' já existe.")
+        else:
+            cadastrar_usuario(nome, username, senha, email, perfil)
+            st.success(f"✅ Usuário '{username}' cadastrado com sucesso.")
