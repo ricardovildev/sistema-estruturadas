b61ddb7fdc766a606e320e996a91f4f6e5fb1160
Jose Ricardo <ricardoviljunior@gmail.com>
2025-09-27 13:35:02 -0300

    Ajuste no codigo de forma geral

--- backend/conexao.py
+++ backend/conexao.py
@@ -1,28 +1,21 @@
+import streamlit as st
 from sqlalchemy import create_engine
-import os
 import ssl
-from dotenv import load_dotenv
 
-load_dotenv()
+usuario = st.secrets["DBUSER"]
+
 
 def conectar():
-    usuario = os.getenv("DB_USER")
-    senha = os.getenv("DB_PASSWORD")
-    host = os.getenv("DB_HOST")
-    porta = os.getenv("DB_PORT", "3306")
-    banco = os.getenv("DB_NAME")
-
-    print("Conectando em:", host, banco)
-
-    url = f"mysql+pymysql://{usuario}:{senha}@{host}:{porta}/{banco}"
+    usuario = st.secrets["DBUSER"]
+    senha = st.secrets["DBPASSWORD"]
+    host = st.secrets["DBHOST"]
+    porta = int(st.secrets["DBPORT"])
+    banco = st.secrets["DBNAME"]
 
     ssl_context = ssl.create_default_context()
     ssl_context.check_hostname = False
     ssl_context.verify_mode = ssl.CERT_NONE
 
-    engine = create_engine(
-        url,
-        connect_args={"ssl": ssl_context}
-    )
-
+    url = f"mysql+pymysql://{usuario}:{senha}@{host}:{porta}/{banco}"
+    engine = create_engine(url, connect_args={'ssl': ssl_context})
     return engine
